46168f3920a8c59f79e639a3a072e6eb1c265a63
Samuel Njenga <snjenga704@gmail.com>
2020-06-05 21:20:55 +0300

    Added RodCuttingProblem.java
    Solution to RodCuttingProblem

--- src/KnapsackProblem.java
+++ src/KnapsackProblem.java
@@ -41,9 +41,9 @@ class KnapsackProblem {
 
     // Driver program to test above function
     public static void main(String args[]) {
-        int val[] = new int[]{60, 100, 120};
-        int wt[] = new int[]{10, 20, 30};
-        int W = 50;
+        int val[] = new int[]{50, 100, 130};
+        int wt[] = new int[]{10, 20, 40};
+        int W = 80;
         int n = val.length;
         System.out.println(knapSack(W, wt, val, n));
     }
